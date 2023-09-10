98fa83bfb3072e11204625be3bf288856c1e9898
yzoug <zoug@protonmail.ch>
2023-09-10 17:14:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# minac - `minac` Is Not A Chessboard
+
+## What `minac` is
+
+A text-based middleware to Lichess's board API, written in Rust.
+
+Currently, the `minac` binary waits for input from `stdin`:
+
+* white makes first move as SAN (Standard Algebraic Notation)
+* chess game status is recorded using the `chess` crate
+* for wrong move, refuses the input, asks to do another one
+* when checkmate is on the board, displays the full game as PGN.
+
+To compile it locally, you need my fork of the `chess` crate:
+
+* This [PR adds support for PGN](https://github.com/jordanbray/chess/pull/71).
+* Checkout the branch and put the repo as `../chess-yzoug-fork`: see `Cargo.toml`.
+
+To use the online mode, a Lichess token with the `challenge:read/write` and `board:play` permissions is needed: write this token in the `token.secret` file at the root of the project, before running `cargo run`.
+
+This software is still in alpha, and I don't have experience with Rust. You've been warned ;).
+
+## What `minac` wants to be
+
+A 3D printed chess clock that makes it easy to play on a physical board connected to Lichess, and can serve as a game notation device that create studies, analyzes games etc.
+
+The final object will probably be:
+
+* A Raspberry Pi Zero
+* A speaker
+* A 3D printed casing
+* Small LCD screen
+* Buttons connected to the a GPIO pin (voltage dividers)
+    * two clock buttons
+    * ranks: from 1 to 8 included, optionally 0 and 9 also for time settings
+    * files: A to H included
+    * pieces: king, queen, rook, bishop, knight
+    * online mode button (lichess logo), offline mode button (stockfish logo)
+
+All the buttons, except the clock buttons, could be replaced by a touchscreen. Everything should be handled by the `minac` binary.
+
+Wanted features:
+
+* Audio clips (confirm inputted moves, error when wrong move is pressed...).
+* Online mode
+    * Lichess Board API to play using an online account
+    * Choose ranked or friendly and time settings
+    * Play against online bots
+* Offline mode against bot
+    * Play against Stockfish and setting its level, possibly other bots
+    * Choose side, time settings
+* Offline 2 players mode
+    * Standard chess clock features
+    * Save game as PGN
+    * Open studies on Lichess
+
+This is v5 I'd say, we're not exactly there.
+
+## Licence
+
+This project is licensed with the Affero GPLv3. See LICENSE.md for the full license, or [this page](https://choosealicense.com/licenses/agpl-3.0/) for a quick recap.
+
+## Contributing
+
+Feel free to send your enhancements and patches as PRs, or open issues.
+
+## Roadmap
+
+Currently working on:
+
+* Online game on Lichess. Objective: play a full game on Lichess using the board:play API through the current commandline interface
+    * Try the token with curl: `curl https://lichess.org/api/account -H "Authorization: Bearer {token}"`
+    * board:play primarily: [doc](https://lichess.org/api#tag/Board)
+
+## About me
+
+Lichess: @zoug
+Mastodon: @zoug@infosec.exchange
+Github: yzoug
+https://zoug.top
+
---
+++ src/main.rs
@@ -0,0 +1,165 @@
+extern crate chess;
+use chess::{ChessMove, Game};
+
+extern crate lichess_api;
+use lichess_api::client::LichessApi;
+use lichess_api::model::account;
+use lichess_api::model::board;
+use lichess_api::error::Result;
+
+use lichess_api::model::board::stream::events::Event;
+use tokio::time::{sleep, Duration};
+use tokio::spawn;
+
+use futures::stream::StreamExt;
+
+use reqwest::ClientBuilder;
+
+use std::io;
+use std::fs;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let token = fs::read_to_string("./token.secret")
+        .expect("Can't read token.secret file: your Lichess token is needed");
+
+    let result = match get_game_mode() {
+        0 => Ok(offline_game()),
+        1 => online_game(&token).await,
+        _ => Ok({
+            println!("Option not supported.");
+        })
+    };
+
+    match result {
+        Ok(_) => Ok(println!("Goodbye!")),
+        Err(e) => {
+            println!("An error happenned: {:?}", e);
+            Err(e)
+        }
+    }
+}
+
+fn offline_game() {
+    let mut game = Game::new();
+
+    // while the game is still ongoing
+    while game.result().is_none() {
+        let current_board = game.current_position();
+        println!("Current FEN: {}", current_board);
+
+        println!("{:?} to move. Enter the SAN move (ex: Nf3):", game.side_to_move());
+
+        let mut next_move_str = String::new();
+        io::stdin()
+            .read_line(&mut next_move_str)
+            .expect("IO Eroor: failed to read line");
+
+        // convert the SAN to a valid move
+        // will repeat the loop if the move is not valid
+        let next_move = match ChessMove::from_san(&current_board, &next_move_str) {
+            Ok(m) => m,
+            Err(e) => {
+                println!("Problem with the chess move, try again. Error: {}", e);
+                continue;
+            },
+        };
+
+        // make the move
+        game.make_move(next_move);
+
+    }
+
+    println!("The game is over. Complete PGN for analysis: {}", game);
+}
+
+
+fn get_game_mode() -> u8 {
+
+    println!("minac - minac Is Not A Chessboard
+    -----------------------
+
+    All moves must be in SAN (Standard Algebraic Notation).
+
+    Choose either offline (0) or online (1) game:
+    > ");
+
+    let mut choice = String::new();
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("IO Eroor: failed to read line");
+
+    choice.trim().parse().expect("Please type a number")
+
+}
+
+fn handle_event(event: Event) {
+    println!("Event received");
+}
+
+async fn stream_event_loop(auth_header: String) -> Result<()> {
+    let client = ClientBuilder::new().build().unwrap();
+    let api = LichessApi::new(client, Some(auth_header));
+    println!("Hello from stream_event_loop");
+    let stream_request = board::stream::events::GetRequest::new();
+    let mut stream = api.board_stream_incoming_events(stream_request).await?;
+    println!("Spawned stream");
+    while let Some(event) = stream.next().await {
+        match event {
+            Ok(e) => {
+                println!("Event received");
+                handle_event(e);
+            }
+            Err(e) => {
+                println!("Error produced: {e}");
+            }
+        };
+    }
+    Ok(())
+}
+
+async fn online_game(token: &str) -> Result<()> {
+    let mut game = Game::new();
+
+    let client = ClientBuilder::new().build().unwrap();
+
+    let auth_header = String::from(token).trim().to_string();
+    let api = LichessApi::new(client, Some(auth_header.clone()));
+
+    // seperate runtime for streaming events
+    spawn(stream_event_loop(auth_header.clone()));
+
+    // display current profile info
+    let profile_request = account::profile::GetRequest::new();
+    let profile = api.get_profile(profile_request).await?;
+    println!("{:?}", profile);
+
+    // create a game against a bot
+    // while the game is still ongoing
+    while game.result().is_none() {
+        let current_board = game.current_position();
+
+        println!("{:?} to move. Enter the SAN move (ex: Nf3):", game.side_to_move());
+
+        let mut next_move_str = String::new();
+        io::stdin()
+            .read_line(&mut next_move_str)
+            .expect("IO Eroor: failed to read line");
+
+        // convert the SAN to a valid move
+        // will repeat the loop if the move is not valid
+        let next_move = match ChessMove::from_san(&current_board, &next_move_str) {
+            Ok(m) => m,
+            Err(e) => {
+                println!("{}", e);
+                continue;
+            },
+        };
+
+        // make the move
+        game.make_move(next_move);
+
+    }
+
+    Ok(println!("The game is over. Complete PGN for analysis: {}", game))
+}
